4dbd733a1fbf65648879372662c1d215e0f063c2
Sebastian Torres <seba.torres.dev@gmail.com>
2021-10-05 12:54:57 -0300

    Helper functions and unit testing step3 (#6)
    * Blog List, Step1
    
    * Blog List, Step 2
    
    * helper functions and unit testing, step1
    
    * helper functions and unit testing, step 2
    
    * helper functions and unit testing, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,12 +3,29 @@ const dummy = blogs => {
 }
 
 const totalLikes = blogs => {
-  const reducer = blogs.reduce(function (a,b) { return a + b.likes; }, 0)
+  const reducer = blogs.reduce(function (a, b) {
+    return a + b.likes
+  }, 0)
 
   return reducer
 }
 
+const favoriteBlog = blogs => {
+  if (blogs.length !== 0) {
+    const favoriteBlog = Math.max(...blogs.map(item => item.likes))
+    const result = {
+      title: blogs.find(item => item.likes === favoriteBlog).title,
+      author: blogs.find(item => item.likes === favoriteBlog).author,
+      likes: blogs.find(item => item.likes === favoriteBlog).likes
+    }
+    return result
+  } else {
+    return {}
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
