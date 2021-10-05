f3ee51018eac56ae3609e1d6c3632b1e1fb75f7f
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-10-05 10:33:04 -0300

    helper functions and unit testing, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const reducer = blogs.reduce(function (a,b) { return a + b.likes; }, 0)
+
+  return reducer
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
