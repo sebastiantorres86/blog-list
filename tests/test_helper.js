f3770fa6ff8da80a0dc12791fe6f7f4eb4078c1a
Sebastian Torres <seba.torres.dev@gmail.com>
2021-10-14 14:03:47 -0300

    Blog list expansions step1 (#14)
    * Blog List, Step1
    
    * Blog List, Step 2
    
    * helper functions and unit testing, step1
    
    * helper functions and unit testing, step 2
    
    * helper functions and unit testing, step 3
    
    * helper functions and unit testing, step 4
    
    * helper functions and unit testing, step 5
    
    * Blog List Tests, step1
    
    * Blog List Tests, step1
    
    * Blog List Tests, Step 2
    
    * Blog List Tests, Step 3
    
    * Blog List Tests, Step 4
    
    * Blog List Tests, Step 5
    
    * Blog List Expansions, Step1

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -2,21 +2,17 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    _id: '5a422b3a1b54a676234d17f9',
     title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
+    likes: 12
   },
   {
-    _id: '5a422b891b54a676234d17fa',
     title: 'First class tests',
     author: 'Robert C. Martin',
     url:
       'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
+    likes: 10
   }
 ]
 
